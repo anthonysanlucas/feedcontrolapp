4d46286b2880df361b75e96f43b01935589cd6b4
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-07 21:47:35 -0500

    Add funcionality to use vehicle capacity

--- ec.com.naturisa.mobile.feedcontrol/Features/Distribution/ViewModels/NewPoolTransferTwoStepViewModel.cs
+++ ec.com.naturisa.mobile.feedcontrol/Features/Distribution/ViewModels/NewPoolTransferTwoStepViewModel.cs
@@ -9,6 +9,9 @@
         private List<string> availablePools;
 
         [ObservableProperty]
+        private int vehicleCapacity;
+
+        [ObservableProperty]
         private ObservableCollection<PoolTransferRow> poolTransferRows;
 
         [ObservableProperty]
@@ -17,16 +20,17 @@
         public NewPoolTransferTwoStepViewModel(IToastService toastService)
             : base(toastService)
         {
-            availableProducts = new List<string>
+            VehicleCapacity = 165;
+
+            AvailableProducts = new List<string>
             {
                 "AQUAXCEL MW 424 SLD STARTER 0.8 MM",
                 "AQUAXCEL MW 424 SLD STARTER 0.6 MM",
                 "AQUAXCEL SLD 1.2 MM"
             };
 
-            availablePools = new List<string> { "MA001", "MA002", "MA003", "MA005" };
+            AvailablePools = new List<string> { "MA001", "MA002", "MA003", "MA005" };
 
-            // Crear filas de transferencias utilizando las piscinas disponibles
             PoolTransferRows = new ObservableCollection<PoolTransferRow>();
 
             PoolTransferRows.Add(
@@ -59,8 +63,17 @@
         }
 
         [RelayCommand]
-        public void AddPoolTransferRow()
+        public async void AddPoolTransferRow()
         {
+            if (RemainingCapacity < 0)
+            {
+                await ToastService.ShowToastAsync(
+                    "No se puede superar la capacidad máxima del vehículo"
+                );
+
+                return;
+            }
+
             PoolTransferRow newRow = new();
             newRow.PropertyChanged += PoolTransferRow_PropertyChanged;
 
@@ -81,18 +94,30 @@
         [RelayCommand]
         async Task GoToNewPoolTransferThreeStep()
         {
+            if (RemainingCapacity < 0)
+            {
+                await ToastService.ShowToastAsync(
+                    "No se puede superar la capacidad máxima del vehículo."
+                );
+
+                return;
+            }
+
             await Shell.Current.GoToAsync(nameof(NewPoolTransferThreeStepView));
         }
 
         public int TotalQuantitySacks =>
-            poolTransferRows.Sum(row => int.TryParse(row.QuantitySacks, out int sacks) ? sacks : 0);
+            PoolTransferRows.Sum(row => int.TryParse(row.QuantitySacks, out int sacks) ? sacks : 0);
 
         public int TotalWeightInKilos => TotalQuantitySacks * 25;
+
+        public int RemainingCapacity => VehicleCapacity - TotalQuantitySacks;
 
         private void UpdateTotals()
         {
             OnPropertyChanged(nameof(TotalQuantitySacks));
             OnPropertyChanged(nameof(TotalWeightInKilos));
+            OnPropertyChanged(nameof(RemainingCapacity));
         }
     }
 

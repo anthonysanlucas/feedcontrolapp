d0357e99d1957d465734b7985687f6546c7e1bf9
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-14 14:42:57 -0500

    Add models

---
+++ ec.com.naturisa.mobile.feedcontrol/Models/FeedTransfer/TransportModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ec.com.naturisa.mobile.feedcontrol.Models.FeedTransfer
+{
+    public class TransportModel
+    {
+        public int Id { get; set; }
+
+        public required string Plate { get; set; }
+    }
+}

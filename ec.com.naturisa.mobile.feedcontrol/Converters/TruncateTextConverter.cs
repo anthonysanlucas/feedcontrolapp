bab6f026e7c2db965179f9fa50df368f0881edad
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-14 11:40:18 -0500

    Create TruncateTextConverter

---
+++ ec.com.naturisa.mobile.feedcontrol/Converters/TruncateTextConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace ec.com.naturisa.mobile.feedcontrol.Converters
+{
+    public class TruncateTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                var words = text.Split(' ');
+                int maxWords = parameter != null ? int.Parse(parameter.ToString()) : 3;
+
+                if (words.Length > maxWords)
+                {
+                    return string.Join(" ", words.Take(maxWords)) + "...";
+                }
+
+                return text;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

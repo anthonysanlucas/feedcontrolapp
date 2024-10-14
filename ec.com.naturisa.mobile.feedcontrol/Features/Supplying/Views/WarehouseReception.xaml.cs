54bd71c9cb8e6774901854f105342dc802f36d5b
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-14 13:36:28 -0500

    Add Supplying module

---
+++ ec.com.naturisa.mobile.feedcontrol/Features/Supplying/Views/WarehouseReception.xaml.cs
@@ -0,0 +1,9 @@
+namespace ec.com.naturisa.mobile.feedcontrol.Features.Supplying.Views;
+
+public partial class WarehouseReception : ContentPage
+{
+	public WarehouseReception()
+	{
+		InitializeComponent();
+	}
+}

b704d57633d1d30ad8ce9b4dfd909c3def8cb114
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-28 19:30:23 -0500

    Add functionality to TransferReception

--- ec.com.naturisa.mobile.feedcontrol/Features/Supplying/ViewModels/TransferReceptionViewModel.cs
+++ ec.com.naturisa.mobile.feedcontrol/Features/Supplying/ViewModels/TransferReceptionViewModel.cs
@@ -1,11 +1,69 @@
 ﻿namespace ec.com.naturisa.mobile.feedcontrol.Features.Supplying.ViewModels
 {
-   public partial class TransferReceptionViewModel : BaseViewModel
+    [QueryProperty(nameof(SelectedTransfer), nameof(SelectedTransfer))]
+    public partial class TransferReceptionViewModel : BaseViewModel
     {
+        private readonly ISupplierTransferService _supplierTransferService;
 
-        public TransferReceptionViewModel(IToastService toastService) : base(toastService)
+        [ObservableProperty]
+        private SupplierTransferResponse selectedTransfer;
+
+        public TransferReceptionViewModel(IToastService toastService, ISupplierTransferService supplierTransferService) : base(toastService)
         {
-            
+            _supplierTransferService = supplierTransferService;
         }
+
+        #region Commands
+        [RelayCommand]
+        async Task ReceptTransfer()
+        {
+            IsNotBusy = false;
+            IsBusy = true;
+            IsRefreshing = false;
+
+            try
+            {
+                List<ReceivedDetails> receivedDetails = new List<ReceivedDetails>();
+
+                foreach (var item in SelectedTransfer.SupplierTransferDetails)
+                {
+                    var obj = new ReceivedDetails
+                    {
+                        IdSupplierTransferDetail = item.IdSupplierTransferDetail,
+                        QuantityReceivedSacks = item.EquivalenceSacks
+                    };
+
+                    receivedDetails.Add(obj);
+                }
+
+                var response = await _supplierTransferService.ChangeStatus(SelectedTransfer.IdSupplierTransfer, SupplierTransferConstants.Delivered, SelectedTransfer.Observation, receivedDetails);
+
+                if (response != null && response.Code != 200)
+                {
+                    throw new Exception(response.Message);
+                }
+                await ToastService.ShowToastAsync(
+                    "Operación realizada con éxito.",
+                    ToastDuration.Long
+                );         
+
+                await Shell.Current.GoToAsync($"//{nameof(WarehouseTransferView)}");
+            }
+            catch (Exception e)
+            {
+                await ToastService.ShowToastAsync(
+                    e.Message,
+                    ToastDuration.Long
+                );
+            }
+            finally
+            {
+                IsBusy = false;
+                IsNotBusy = true;
+
+            }
+
+        }
+        #endregion
     }
 }

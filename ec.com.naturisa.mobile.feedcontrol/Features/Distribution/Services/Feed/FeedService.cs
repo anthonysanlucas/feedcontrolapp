fb03cdef70bab7e8e4f5c254f1e7cffdddc073b5
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-10-30 15:18:07 -0500

    Add FeedService class and clean up csproj file

---
+++ ec.com.naturisa.mobile.feedcontrol/Features/Distribution/Services/Feed/FeedService.cs
@@ -0,0 +1,17 @@
+﻿namespace ec.com.naturisa.mobile.feedcontrol.Features.Distribution.Services.Feed
+{
+   public class FeedService : BaseHttpService
+    {
+        private static class FeedEndpoints
+        {
+            public const string Feed = $"{ApiConstants.API_FEED_CONTROL}/feed";
+
+            public static string FeedTransferById(int id) => $"{Feed}/{id}/change_status";
+        }
+
+        public FeedService() : base(ApiConstants.API_FEED_CONTROL)
+        {            
+        }
+
+    }
+}

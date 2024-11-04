4ae79c5aac63c3a105fc3f43cfa5c33502bfb305
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-11-04 10:33:20 -0500

    Refactor FeedingPoolViewModel properties and fields

--- ec.com.naturisa.mobile.feedcontrol/Features/Distribution/ViewModels/FeedingPoolViewModel.cs
+++ ec.com.naturisa.mobile.feedcontrol/Features/Distribution/ViewModels/FeedingPoolViewModel.cs
@@ -1,99 +1,21 @@
 ﻿namespace ec.com.naturisa.mobile.feedcontrol.Features.Distribution.ViewModels
 {
     public partial class FeedingPoolViewModel : BaseViewModel
-    {
-        public ObservableCollection<PoolFeedingAndRemainingState> PoolFeedingList { get; set; }
-
+    {        
         [ObservableProperty]
         private ObservableCollection<FeedResponse> feeds;       
 
-        private readonly IFeedService _feedService;
-
         [ObservableProperty]
         private FeedQuery feedQuery;
+       
+        private readonly IFeedService _feedService;
 
         public FeedingPoolViewModel(IToastService toastService, IFeedService feedService)
             : base(toastService)
         {
-            _feedService = feedService;
+            _feedService = feedService;            
 
-            PoolFeedingList = new ObservableCollection<PoolFeedingAndRemainingState>
-                {
-                    new()
-                    {
-                        PoolName = "MA 001",
-                        IsFeeding = true,
-                        IsRemaining = false
-                    },
-                    new()
-                    {
-                        PoolName = "MA 002",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 003",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 004",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 005",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 006",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 007",
-                        IsFeeding = true,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 008",
-                        IsFeeding = false,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 009",
-                        IsFeeding = false,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 010",
-                        IsFeeding = false,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 011",
-                        IsFeeding = false,
-                        IsRemaining = true
-                    },
-                    new()
-                    {
-                        PoolName = "MA 012",
-                        IsFeeding = false,
-                        IsRemaining = true
-                    }
-                };
-
-            feedQuery = new FeedQuery
+            FeedQuery = new FeedQuery
             {
                 Date = new DateTime(2024, 10, 29),
                 StartDate = DateTime.Now,

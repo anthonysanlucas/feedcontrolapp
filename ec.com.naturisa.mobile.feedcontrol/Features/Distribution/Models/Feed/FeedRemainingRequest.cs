d45fa37f551f80f74ee8a0a8f8f070d9571a1e09
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-11-06 21:23:16 -0500

    Remove validation and Observation from FeedRemainingRequest

--- ec.com.naturisa.mobile.feedcontrol/Features/Distribution/Models/Feed/FeedRemainingRequest.cs
+++ ec.com.naturisa.mobile.feedcontrol/Features/Distribution/Models/Feed/FeedRemainingRequest.cs
@@ -1,17 +1,10 @@
 ﻿namespace ec.com.naturisa.mobile.feedcontrol.Features.Distribution.Models.Feed
 {
     public class FeedRemainingRequest
-    {
-        [Required(ErrorMessage = "El ID es obligatorio.")]
-        [Range(0, long.MaxValue, ErrorMessage = "El ID debe ser un número mayor o igual que 0.")]
-        public long IdFeed { get; set; }
-
-        [Range(0, long.MaxValue, ErrorMessage = "La cantidad de sacos restantes en tolvas debe ser un número mayor o igual que 0.")]
+    {        
+        public long IdFeed { get; set; }     
         public int? SacksRemainingHoppers { get; set; }
-
-        [Range(0, long.MaxValue, ErrorMessage = "La cantidad de tolvas dañadas debe ser un número mayor o igual que 0.")]
         public int? FailedHoppers { get; set; }
-
         public string? Observation { get; set; }
 
 

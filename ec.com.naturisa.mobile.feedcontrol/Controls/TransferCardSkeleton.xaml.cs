0bbccf7387f612563df21362d12ef1ec6b9f816b
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-11-08 15:12:41 -0500

    Add transferCardSkeleton

---
+++ ec.com.naturisa.mobile.feedcontrol/Controls/TransferCardSkeleton.xaml.cs
@@ -0,0 +1,9 @@
+namespace ec.com.naturisa.mobile.feedcontrol.Controls;
+
+public partial class TransferCardSkeleton : ContentView
+{
+	public TransferCardSkeleton()
+	{
+		InitializeComponent();
+	}
+}

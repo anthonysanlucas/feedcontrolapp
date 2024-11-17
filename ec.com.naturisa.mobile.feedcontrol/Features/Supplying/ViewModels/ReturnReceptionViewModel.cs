e2cb97a9524712844221794c360759f3520bbd3c
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-11-17 16:25:05 -0500

    Enhance ReturnReception with new properties and UI elements
    Updated ReturnReceptionViewModel:
    - Added ObservableCollection<FeedTransferModel> returns.
    - Added FeedTransferQuery transferQuery.
    - Introduced _feedTransferService for service interaction.
    - Modified constructor to accept IFeedTransferService.
    - Added GetReturns method with [RelayCommand].
    
    Updated ReturnReceptionView.xaml:
    - Added XML namespaces for components, controls, and models.
    - Introduced RefreshView for pull-to-refresh functionality.
    - Added CollectionView to display return transfers.
    - Defined DataTemplate for CollectionView items.
    - Included EmptyView for no return transfers message.
    - Added TransferCardSkeleton for loading state.

--- ec.com.naturisa.mobile.feedcontrol/Features/Supplying/ViewModels/ReturnReceptionViewModel.cs
+++ ec.com.naturisa.mobile.feedcontrol/Features/Supplying/ViewModels/ReturnReceptionViewModel.cs
@@ -5,8 +5,23 @@ public partial class ReturnReceptionViewModel : BaseViewModel
     [ObservableProperty]
     private ObservableCollection<FilterStatus> filterStatuses;
 
-    public ReturnReceptionViewModel(IToastService toastService) : base(toastService)
+    [ObservableProperty]
+    private ObservableCollection<FeedTransferModel> returns;
+
+    [ObservableProperty]
+    private FeedTransferQuery transferQuery;
+
+    private readonly IFeedTransferService _feedTransferService;
+
+    public ReturnReceptionViewModel(IFeedTransferService feedTransferService, IToastService toastService) : base(toastService)
     {
+        _feedTransferService = feedTransferService;
+
+        TransferQuery = new FeedTransferQuery
+        {
+            Type = Const.Types.FeedTransferType.Return
+        };
+
         FilterStatuses = new ObservableCollection<FilterStatus>
         {
             new FilterStatus { Status = "TODOS", IsSelected = true },
@@ -15,6 +30,8 @@ public partial class ReturnReceptionViewModel : BaseViewModel
             new FilterStatus { Status = "EN RUTA" },
             new FilterStatus { Status = "ENTREGADO" }
         };
+
+        GetReturns();
     }
 
     [RelayCommand]
@@ -23,6 +40,39 @@ public partial class ReturnReceptionViewModel : BaseViewModel
         foreach (var filter in FilterStatuses)
         {
             filter.IsSelected = filter.Status == status;
+        }
+    }
+
+    [RelayCommand]
+    async Task GetReturns()
+    {
+        IsNotBusy = false;
+        IsBusy = true;
+        IsRefreshing = false;
+
+        try
+        {
+            var response = await _feedTransferService.GetFeedTransfers(TransferQuery);
+
+            if (response != null && response.Data != null && response.Data.Data.Any())
+            {
+                var feedTransferModels = response.Data.Data;
+
+                Returns = new ObservableCollection<FeedTransferModel>(feedTransferModels);
+            }
+            else
+            {
+                Returns?.Clear();
+            }
+        }
+        catch (Exception ex)
+        {
+            await ToastService.ShowToastAsync("Ha ocurrido un error, intente nuevamente.");
+        }
+        finally
+        {
+            IsBusy = false;
+            IsNotBusy = true;
         }
     }
 }

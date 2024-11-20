329f39c85e42f49d87e250add98f172baf476b39
Antony San Lucas <asanlucas@naturisa.com.ec>
2024-11-20 09:40:31 -0500

    Change selectedPool type in PoolTransferTwoStepSelectionModel
    Updated the type of the `selectedPool` property from
    `FeedTransferDetailPoolModel?` to `PoolsResponse?` in the
    `PoolTransferTwoStepSelectionModel` class. This change reflects
    an update in the underlying data model or a refinement in the
    type used to represent the selected pool in the application.

--- ec.com.naturisa.mobile.feedcontrol/Models/FeedTransfer/PoolTransferTwoStepSelectionModel.cs
+++ ec.com.naturisa.mobile.feedcontrol/Models/FeedTransfer/PoolTransferTwoStepSelectionModel.cs
@@ -6,7 +6,7 @@
         public FeedTransferDetailModel? selectedProduct;
 
         [ObservableProperty]
-        public FeedTransferDetailPoolModel? selectedPool;
+        public PoolsResponse? selectedPool;
 
         [ObservableProperty]
         public int? quantitySacks;
